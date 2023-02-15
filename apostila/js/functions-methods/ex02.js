fdb30f979a5d7b2bd90f00e5938edbc18550b243
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2023-02-14 22:09:03 -0300

    feat: functions-methods/ex02 js and html

---
+++ apostila/js/functions-methods/ex02.js
@@ -0,0 +1,14 @@
+console.log("TeSt OnE".toUpperCase())
+
+console.log("tEsT tWo".toLowerCase())
+
+const list = []
+
+list.push(1)
+list.push(2)
+list.push(3)
+list.push(4)
+
+console.log(list.pop())
+
+console.log(list)
