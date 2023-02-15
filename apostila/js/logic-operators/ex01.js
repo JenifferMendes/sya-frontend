202aef24d0c62d9e01508c5233ce170f47dd4414
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2023-02-14 21:38:57 -0300

    feat: logic-operators/ex01 js and html

---
+++ apostila/js/logic-operators/ex01.js
@@ -0,0 +1,23 @@
+if (5 == 5) {
+    console.log("5 == 5")
+}
+
+if (5 != 5) {
+    console.log("5 != 5")
+}
+
+if (5  > 5) {
+    console.log("5  > 5")
+}
+
+if (5  < 5) {
+    console.log("5  < 5")
+}
+
+if (5 >= 5) {
+    console.log("5 >= 5")
+}
+
+if (5 <= 5) {
+    console.log("5 <= 5")
+}
