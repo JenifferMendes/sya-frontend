4e6c79644259db30ba5ce824c9907e1c4b87e8fb
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2023-02-16 20:56:23 -0300

    feat: primitive-data-types/ex01 js and html

---
+++ apostila/js/primitive-data-types/ex01.js
@@ -0,0 +1,50 @@
+// Boolean
+const boolTrue = true
+const boolFalse = false
+
+console.log(boolTrue)
+console.log(boolFalse)
+
+// Null
+
+const typeNull = null
+
+console.log(typeNull)
+
+// Undefined
+
+const typeUndefined = undefined
+
+console.log(typeUndefined)
+
+// Number
+
+const numberInt = 64
+const numberFloat = 64.1
+const numberBigInt = 64n
+
+console.log(numberInt)
+console.log(numberFloat)
+console.log(numberBigInt)
+
+// NaN
+
+const typeNan = NaN
+
+console.log(typeNan)
+
+// String
+
+const typeString = "test"
+
+console.log(typeString)
+
+// Symbol
+
+const typeSymbol1 = Symbol(1)
+const typeSymbol2 = Symbol("2")
+const typeSymbol3 = Symbol(3.1)
+
+console.log(typeSymbol1)
+console.log(typeSymbol2)
+console.log(typeSymbol3)
