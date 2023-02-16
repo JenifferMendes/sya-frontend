91aa10f56ec91d98a2cc6614b2914641fbbcc572
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2023-02-15 21:45:03 -0300

    feat: event-manipulation/ex03 js and html

---
+++ apostila/js/event-manipulation/ex03.js
@@ -0,0 +1,9 @@
+const funcOnMouseOver = () => {
+    console.log("mouse over")
+}
+
+const h1 = document.querySelector("#my-h1")
+
+h1.addEventListener("mouseover", () => {
+    console.log("mouse over")
+})
